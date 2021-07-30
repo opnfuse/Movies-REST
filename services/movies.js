a4d0ae3f9d47bc353eb662528e8f569a4a375548
Mario Quevedo <opnfuso@gmail.com>
2021-07-30 12:27:26 -0500

    delete husky

--- services/movies.js
+++ services/movies.js
@@ -3,12 +3,14 @@ import moviesMock from '../utils/mocks/movies.js';
 class MoviesService {
   async getMovies(tags) {
     const movies = await Promise.resolve(moviesMock);
+    console.log(tags);
 
     return movies || [];
   }
 
   async getMovie(id) {
     const movie = await Promise.resolve(moviesMock[0]);
+    console.log(id);
 
     return movie || {};
   }
@@ -21,12 +23,14 @@ class MoviesService {
 
   async updateMovie(id) {
     const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+    console.log(id);
 
     return updatedMovieId || '';
   }
 
   async deleteMovie(id) {
     const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+    console.log(id);
 
     return deletedMovieId || '';
   }
